6df022dd0a446c8818ac64ef1e2d6bd539b706d9
TejasPrabhu21 <tejasprabhu21@gmail.com>
2024-11-23 12:06:42 +0530

    Initial setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { withFetch } from '@angular/common/http';
+import {
+  provideTanStackQuery,
+  QueryClient,
+  withDevtools,
+} from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
+  ],
 };
